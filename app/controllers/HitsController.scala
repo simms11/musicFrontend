66e0e2ca96e1dde893e74d86415f8690ae3d6001
simms11 <alsahid.simms@accenture.com>
2020-11-19 12:52:09 +0000

    Modified addHit method

--- app/controllers/HitsController.scala
+++ app/controllers/HitsController.scala
@@ -1,22 +1,32 @@
 package controllers
 
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import javax.inject.Inject
 import models.Hit
+import models.Writeables.jsonWritable
+import play.api.i18n.{I18nSupport, MessagesProvider}
+import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import models.Writeables.jsonWritable
-import play.api.libs.json.Json
-import models.Hit
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.util.{Failure, Success}
 
 
-class HitsController @Inject()(ws: WSClient, cc: ControllerComponents) extends AbstractController(cc) {
+case class HitsController @Inject()(ws: WSClient, cc: ControllerComponents, ms : MessagesProvider) extends AbstractController(cc) with I18nSupport {
+  implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "SingleRequest")
+  implicit val executionContext: ExecutionContextExecutor = system.executionContext
 
-  def  index()= Action { implicit request =>
-    return Ok(views.html.getHit.render(Hit.form()));
+  def  index()= Action.async { implicit request =>
+    ws.url("http://localhost:9000/hits/3").get().map { response =>
+      val hit = response.json.as[Hit]
+      Ok(views.html.getHit(Hit.form.fill(hit))(request,ms))
+    }
   }
-
   def getHit() = Action.async {
     ws.url("http://localhost:9000/hits/3").get().map { response =>
       Ok(response.json.as[Hit])
@@ -29,4 +39,20 @@ class HitsController @Inject()(ws: WSClient, cc: ControllerComponents) extends A
       Ok(response.json.as[Hit])
     }
 }
+
+  def addHit(newHit: Hit): Any = Action(parse.json) { implicit request =>
+    val responseFuture: Future[HttpResponse] =
+      Http(system).singleRequest(
+        HttpRequest(
+          uri = "http://localhost:9000/hits",
+          method = "POST",
+          entity = ("application/json", newHit),
+          protocol = "HTTP/1.0"
+        ))
+    responseFuture
+      .onComplete {
+        case Success(res) => Ok(res)
+        case Failure(_) => sys.error("something wrong")
+      }
+  }
 }
--- build.sbt
+++ build.sbt
@@ -2,9 +2,17 @@ name := """musicFrontend"""
 organization := "com.example"
 
 version := "1.0-SNAPSHOT"
-
+val akkaVersion = "2.5.26"
 lazy val root = (project in file(".")).enablePlugins(play.sbt.PlayScala).settings(
   scalaVersion := "2.13.3",
 libraryDependencies ++= Seq(guice, ws)
 )
 
+val akkaHttpVersion = "10.1.11"
+libraryDependencies ++= Seq(
+  // akka streams
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+  // akka http
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
+)
